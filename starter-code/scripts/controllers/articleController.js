39a7e33c33b94c980b7f7aa3a0448ecc6166581d
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-23 11:53:33 -0700

    added comments for the first two functions loadbyauthor and loadbyid

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -4,10 +4,16 @@
   Article.createTable();  // Ensure the database table is properly initialized
 
   articlesController.index = function(ctx, next) {
-    articleView.index(ctx.articles);
+    articleView.index(ctx.articles);//don't need next as is the last method in our call chain
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*this method sets the variable articleData to a function which takes an article
+  for an arugment. this function sets a property on context object called
+  articles and the value of this property to the article paseed in.
+  the article data function then calls the next function, passed in by loadById.
+  the next() is articleController.index method which envokes the articleView.index
+  method with the parameter of ctx.articles.*/
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -18,6 +24,11 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*this method sets the variable authorData to a function which takes in the variable
+  articlesByAuthor and assigns it to the articles property on the ctx object.
+  Then the next() callback function is envoked. the next() is articleController.index method which envokes the articleView.index
+  method with the parameter of ctx.articles.
+  */
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -40,7 +51,7 @@
   // COMMENT: What does this method do?  What is it's execution path?
   articlesController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
-      ctx.articles = Article.all;
+      ctx.articles = Article.all;//assign array into ctx object
       next();
     };
 
